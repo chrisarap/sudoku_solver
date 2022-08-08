2ca5d94288a435ae7c96767047f914b3cadf5499
chrisarap <carteaga460@gmail.com>
2022-08-07 20:17:32 -0400

    start to solve sudoku puzzle

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,6 +1,17 @@
 class SudokuSolver {
 
   validate(puzzleString) {
+    if (puzzleString == undefined) {
+      return {error: 'Required field missing'};
+    } else if(puzzleString.length != 81) {
+      return {error: 'Expected puzzle to be 81 characters long'};
+    } else {
+      let rgx = /[^0-9\.]/;
+      if(rgx.test(puzzleString)){
+        return {error: 'Invalid characters in puzzle'};
+      }
+    }
+    this.solve(puzzleString);
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
@@ -16,9 +27,32 @@ class SudokuSolver {
   }
 
   solve(puzzleString) {
-    
+    let letters = 'ABCDEFGHI';
+    let arr = {};
+    let count = 0;
+
+
+    for (var i = 0; i < 9; i++) {
+      for (var j = 1; j <= 9; j++) {
+        let myKey = `${letters[i] + j}`;
+        Object.assign(arr, {[myKey]: (puzzleString[count] == '.' ? [1,2,3,4,5,6,7,8,9] : Number(puzzleString[count]))});
+        count++;
+      }
+    }
+
+    //let newArr = Object.entries(arr);
+
+    for (var variable in arr) {
+      if (typeof arr[variable] != 'number') {
+        console.log(variable, arr[variable]);
+      }
+
+    }
   }
+
+
+
+
 }
 
 module.exports = SudokuSolver;
-
--- routes/api.js
+++ routes/api.js
@@ -3,16 +3,24 @@
 const SudokuSolver = require('../controllers/sudoku-solver.js');
 
 module.exports = function (app) {
-  
+
   let solver = new SudokuSolver();
 
   app.route('/api/check')
     .post((req, res) => {
+      let myObj = {};
 
+      solver.validate(req.body.puzzle) ? myObj = solver.validate(req.body.puzzle) : {};
+
+      return res.send(myObj);
     });
-    
+
   app.route('/api/solve')
     .post((req, res) => {
+      let myObj = {};
 
+      solver.validate(req.body.puzzle) ? myObj = solver.validate(req.body.puzzle) : {};
+
+      return res.send(myObj);
     });
 };
